e03e79e932ca73c0c7d9493f3e84df86f6b81065
radekkrecz <radekkrecz@gmail.com>
2024-02-16 18:46:57 +0100

    Dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Ewa";
+bool kobieta = false;
+int wiek = 10;
+
+if (kobieta && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (kobieta && imie == "Ewa" && wiek == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (!kobieta && wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
