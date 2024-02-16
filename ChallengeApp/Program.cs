67a116eb88807068953eea56b78c9035789ab9c5
radekkrecz <radekkrecz@gmail.com>
2024-02-16 19:30:15 +0100

    Poprawka nazw zmiennych na nazwy angielskie

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,16 +1,16 @@
-﻿string imie = "Ewa";
-bool kobieta = false;
-int wiek = 10;
+﻿string name = "Ewa";
+bool women = false;
+int age = 10;
 
-if (kobieta && wiek < 30)
+if (women && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (kobieta && imie == "Ewa" && wiek == 30)
+else if (women && name == "Ewa" && age == 30)
 {
     Console.WriteLine("Ewa, lat 30");
 }
-else if (!kobieta && wiek < 18)
+else if (!women && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
